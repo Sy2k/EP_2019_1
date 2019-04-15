16e0c6c8b14c3e8e8daa79a3e59f31694fe1aca0
ellenbs <49625971+ellenbs@users.noreply.github.com>
2019-04-15 08:04:33 -0300

    Update ep1.py

--- ep1.py
+++ ep1.py
@@ -1,8 +1,4 @@
 # EP 2019-1: Escape Insper
-#
-# Alunos: 
-# - aluno A: Fulano da Silva, fulanos@insper.edu.br
-# - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
 
 def carregar_cenarios():
     cenarios = {
