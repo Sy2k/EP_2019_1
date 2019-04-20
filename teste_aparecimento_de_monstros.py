9e392cbd19d1cb05b339ed251423485cb9535d24
Sy2k <stephaniel@al.insper.edu.br>
2019-04-20 18:44:07 -0300

    a

--- teste_aparecimento_de_monstros.py
+++ teste_aparecimento_de_monstros.py
@@ -5,6 +5,8 @@ with open("HP_HitPoint_Defense(teste).json","r", encoding="utf8") as arquivo:
 	dados = json.load(arquivo)
 with open("char_caract.json", "r", encoding="utf8") as arqz:
 	dados_char = json.load(arqz)
+with open("premios.json","r",encoding="utf8") as arquivoL:
+    data = json.load(arquivoL)
 
 def aparecer_monstros(dados):
 	lista_Mons = []
@@ -27,6 +29,7 @@ def batalha():
 			villain['HP'] = villain['HP'] - (char['HitPoint'] - villain['defesa'])
 			if villain['HP'] <=0:
 				resultado = "O jogador ganhou a batalha"
+				#premio = True 
 				return resultado
 		elif char['HitPoint'] <= villain['defesa']:  # Quando  o poder de ataque do jogador for
 		    villain['HP'] = villain['HP']   # menor igual do que a defesa do oponente, a HP do Oponente nao se altera
@@ -36,12 +39,13 @@ def batalha():
 		elif villain['HitPoint'] > char['defesa']:
 			char['HP'] = char['HP'] - (villain['HitPoint'] - char['defesa'])
 			if char['HP'] <=0:
+				#premio = False
 				resultado = "O jogador perdeu a batalha"
 				return resultado
 
 		elif villain['HitPoint'] < char['defesa']:
 			char['HP'] = char['HP']
 			resultado = "O monstro não faz nem cocegas"
-			return resultado
-
-print(batalha())
+		return resultado
+def premios(data):
+	
