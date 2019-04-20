a2d76b4ef1c2da76cc4cf5abad1b2577eac7c7fc
Ellen Beatriz Shen <ellenbs@al.insper.edu.br>
2019-04-20 15:31:01 -0300

    .

--- ep1.py
+++ ep1.py
@@ -9,10 +9,10 @@ import json
 
 with open('cenarios.json', 'r', encoding="utf8") as arquivo_c:
     cenario = json.load(arquivo_c)
-
-
-with open('lista itens totais.json', 'r', encoding="utf8") as arquivo_i:
+with open('lista_itens_totais.json', 'r', encoding="utf8") as arquivo_i:
     lista = json.load(arquivo_i)
+with open('teletransporte.json', 'r', encoding='utf8') as arquivo_t:
+    lista_tele = json.load(arquivo_t)
 
 def carregar_cenarios():
     cenarios = cenario
@@ -22,14 +22,19 @@ def carregar_cenarios():
 def inventario():
     todos_itens = lista
     inventario_slots = []*2
-    z = input("Qual item deseja pegar")
-    while z in opcao_item:
-        for i in inv:
-            x = i.split()  # Separa.
-            h="".join(z)  # Junta tudo de novo com um espaco.
-        if not h in inv:
-            inv.append(h)  # Adiciona elementos que nao estao presentes na lista
-    return todos_itens, inventario_slots, inv
+    return todos_itens, inventario_slots
+#    z = input("Qual item deseja pegar")
+#    while z in opcao_item:
+#        for i in inv:
+#            x = i.split()  # Separa.
+#            h="".join(z)  # Junta tudo de novo com um espaco.
+#       if not h in inv:
+#            inv.append(h)  # Adiciona elementos que nao estao presentes na lista
+#    return todos_itens, inventario_slots, inv
+def teletransporte():
+    lista_t = lista_tele
+    nome_cenario_teletransporte = "teletransporte"
+    return lista_t,nome_cenario_teletransporte
 
 def main():
     init(autoreset=True)
@@ -49,17 +54,19 @@ def main():
 
     cenarios, nome_cenario_atual = carregar_cenarios()
     todos_itens,inventario_slots = inventario()
+    lista_t, nome_cenario_teletransporte = teletransporte()
 
     game_over = False
     while not game_over:
         inventario_atual= inventario_slots
         cenario_atual = cenarios[nome_cenario_atual]
+        cenario_teletransporte = lista_t[nome_cenario_teletransporte]
+
         print("----------------")
         print(Back.RED + cenario_atual["titulo"])
         print("----------------")
         print(Fore.RED + cenario_atual["descricao"])
         print()
-
         opcoes = cenario_atual['opcoes']
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
@@ -69,32 +76,36 @@ def main():
             for opcao,val in opcoes.items():
                 print(opcao,":",val)
             print(Fore.MAGENTA + "SEU INVENTARIO: {0}".format(inventario_atual))
-            escolhas = cenarios[nome_cenario_atual]['opcoes']
-            escolha = ""
+            escolha_inicial = ""
             print(Fore.CYAN + "O que deseja fazer?")
-            choice = input("")
+            escolha_digitada = input("")
             print()
-            while not choice in escolhas:
-                    print("Opcao inválida!!")
+
+            while not escolha_digitada in opcoes and lista_t:
+                    print(Back.CYAN + "Opcao inválida!!")
                     print("Digite como mostrado, por favor")
                     print()
                     print(Fore.CYAN + "Voce tem as seguintes opcoes:") 
                     for opcao,val in opcoes.items():
                         print(opcao,":",val)
                     print(Fore.CYAN + "O que deseja fazer?")
-                    choice = input("")
+                    escolha_digitada = input("")
                     print()
-            if choice in escolhas:
-                escolha = choice
+
+            if escolha_digitada in opcoes:
+                escolha = escolha_digitada
                 if escolha in opcoes:
                     nome_cenario_atual = escolha
                     if escolha == "item":
                         item_achado = opcoes[escolha]
                         if not item_achado in inventario_atual:
                             inventario_atual.append(item_achado)
-                else:
-                    print("Sua indecisão foi sua ruína!")
-                    game_over = True
+            elif escolha_digitada in lista_t:
+                if escolha == "teletransporte":
+                    teletransporte_escolha = input("para onde deseja se teletransportar? ")
+                    cenario_teletransporte = teletransporte_escolha
+                    nome_cenario_atual = cenario_teletransporte
+            
 
     print("Voce morreu!")
 
