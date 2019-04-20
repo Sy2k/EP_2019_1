c4bf45c3cdcbe1f056c220860c5111656cdf736c
Sy2k <stephaniel@al.insper.edu.br>
2019-04-20 17:34:22 -0300

    teste de batalha

--- teste_aparecimento_de_monstros.py
+++ teste_aparecimento_de_monstros.py
@@ -14,10 +14,32 @@ def aparecer_monstros(dados):
 	return lista_Mons[key]
 
 print (aparecer_monstros(dados))
-"""
+
 def batalha(dados):  
 	n=0
 	list_0 = []
 	list_0.append(Aparecimento_de_Mons(dados))
 print("Você escolheu o item: {0}".format(dados[6]['nome'])) 
-"""
+
+def batalha(dados):
+	dados2 = aparecer_monstros(dados)
+	villain= dados2
+	char = dados[7]
+	while char['HP'] > 0  or villain['HP'] > 0:
+		if char['HitPoint'] > villain['defensa']:
+			villain['HP'] = villain['HP'] - (char['HitPoint'] - villain['defesa'])
+			if villain['HP'] <=0:
+				return "O jogador ganhou a batalha"
+
+		elif char['HitPoint'] <= villain['defesa']:  # Quando  o poder de ataque do jogador for
+		    villain['HP'] = villain['HP']   # menor do que a defesa do oponente, a HPoponente nao se altera
+			
+
+		elif villain['HitPoint'] > char['defesa']:
+			char['HP'] = char['HP'] - (villain['HitPoint'] - char['defesa'])
+			if char['HP'] <=0:
+				return "O jogador perdeu a batalha"
+
+		elif villain['HitPoint'] < char['defesa']:
+			char['HP'] = char['HP']
+
